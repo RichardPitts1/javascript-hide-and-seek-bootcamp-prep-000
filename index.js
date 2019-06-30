cec4d3bb7e0ce17a7bb65d221bb43dc420c2e1ae
Richard Pitts <richardepitts@gmail.com>
2019-06-30 18:59:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(selector) {
-  return document.queryElementsByTagName(selector)[0]
+  return document.querySelector(selector)[0]
   }
