ede53f5327eca409f32c5450134d79c06c102425
Richard Pitts <richardepitts@gmail.com>
2019-06-30 18:58:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(selector) {
-  return document.queryElementsByTagName(selector)
+  return document.queryElementsByTagName(selector )
   }
